e5c454dda4a547f44d99d5fc0fc60e815dc27f79
Rokonio <naelkanada@gmail.com>
2021-05-21 08:28:36 +0200

    Add perspective, camera and mouse movements

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,11 @@ fn main() {
     let height = 42f32;
 
     let block1 = (1f32, 41f32); // Stone
+    let block2 = (0f32, 40f32); // Cobble stone
 
-    let shape = Shape::from_vertices(
+    let mut shape = Shape::from_vertices(
         vec![
-            Vertex::new([-1.0, -0.5, -1.], [block1.0 / width, block1.1 / height]),
+            Vertex::new([-1.0, -0.5, -1.0], [block1.0 / width, block1.1 / height]),
             Vertex::new(
                 [-1.0, 0.5, -1.],
                 [block1.0 / width, (block1.1 + 1.) / height],
@@ -49,8 +50,34 @@ fn main() {
                 [(block1.0 + 1.) / width, block1.1 / height],
             ),
         ],
-        vec![0, 1, 2, 0, 2, 3],
+        vec![
+            0, 1, 2, // Indices
+            0, 2, 3,
+        ],
     );
+
+    shape.add_vertices(
+        vec![
+            Vertex::new([0.0, -0.5, 0.0], [block2.0 / width, block2.1 / height]),
+            Vertex::new(
+                [0.0, 0.5, 0.0],
+                [block2.0 / width, (block2.1 + 1.) / height],
+            ),
+            Vertex::new(
+                [0.5, 0.5, 0.0],
+                [(block2.0 + 1.) / width, (block2.1 + 1.) / height],
+            ),
+            Vertex::new(
+                [0.5, -0.5, 0.0],
+                [(block2.0 + 1.) / width, block2.1 / height],
+            ),
+        ],
+        vec![
+            0, 1, 2, // Indices
+            0, 2, 3,
+        ],
+    );
+
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape.vertices).unwrap();
     let indices = glium::IndexBuffer::new(
         &display,
@@ -58,38 +85,52 @@ fn main() {
         &shape.indices,
     )
     .unwrap();
-    // let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
     let program = default_program(display.clone());
 
-    let mut t = -0.5;
+    let mut camera_pos = (0f32, 0f32, 0f32);
 
     event_loop.run(move |event, _, control_flow| {
-        t += 0.001;
-        if t > 0.5 {
-            t = -0.5;
-        }
-
-        let matrix = glm::translate(&glm::identity(), &glm::vec3(t, 0., 0.));
-        let matrix: [[f32; 4]; 4] = matrix.into();
-
-        let mut target = display.draw();
-        target.clear_color(0.6, 0.9, 0.9, 1.0); // Sky color
-        target
-            .draw(
-                &vertex_buffer,
-                &indices,
-                &program,
-                &uniform! {matrix: matrix, tex: glium::uniforms::Sampler::new(&texture)
-                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Nearest)},
-                &Default::default(),
-            )
-            .unwrap();
-        target.finish().unwrap();
 
         if let glutin::event::Event::WindowEvent { event, .. } = event {
             if let glutin::event::WindowEvent::CloseRequested = event {
                 *control_flow = glutin::event_loop::ControlFlow::Exit;
             }
+        } else if let glutin::event::Event::DeviceEvent { event, ..} = event {
+            if let glutin::event::DeviceEvent::MouseMotion {delta} = event {
+               camera_pos.0 += delta.0 as f32 / 100.;
+                camera_pos.1 += delta.1 as f32 / 100.;
+            }
+            if let glutin::event::DeviceEvent::Key(input) = event {
+                camera_pos.2 += match input.scancode {
+                    17 => 0.1,  // 'z' on azerty
+                    31 => -0.1, // 's' on azerty 
+                    _ => 0.,
+                }
+            }
+        } else if let glutin::event::Event::MainEventsCleared = event {
+            // Render code moved here
+            let model = glm::identity::<f32, 4>();
+            let model: [[f32; 4]; 4] = model.into();
+
+            let view = glm::translate(&glm::identity(), &glm::vec3(camera_pos.0, -camera_pos.1, camera_pos.2));
+            let view: [[f32; 4]; 4] = view.into();
+
+            let projection = glm::perspective(1., degree_to_radian(70.), 0.1, 1000.);
+            let projection: [[f32; 4]; 4] = projection.into();
+
+            let mut target = display.draw();
+            target.clear_color(0.6, 0.9, 0.9, 1.0); // Sky color
+            target
+                .draw(
+                    &vertex_buffer,
+                    &indices,
+                    &program,
+                    &uniform! {model: model, view: view, projection: projection, tex: glium::uniforms::Sampler::new(&texture)
+                    .magnify_filter(glium::uniforms::MagnifySamplerFilter::Nearest)},
+                    &Default::default(),
+                )
+                .unwrap();
+            target.finish().unwrap();
         }
     });
 }
--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,4 @@
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct Vertex {
     pub position: [f32; 3],
     pub tex_coords: [f32; 2],
@@ -14,14 +14,30 @@ impl Vertex {
     }
 }
 
+#[derive(Debug)]
 pub struct Shape {
     pub vertices: Vec<Vertex>,
     pub indices: Vec<u32>,
 }
 
+#[allow(unused)]
 impl Shape {
     pub fn from_vertices(vertices: Vec<Vertex>, indices: Vec<u32>) -> Shape {
         Shape { vertices, indices }
+    }
+
+    pub fn new() -> Shape {
+        Shape {
+            vertices: vec![],
+            indices: vec![],
+        }
+    }
+
+    pub fn add_vertices(&mut self, vertices: Vec<Vertex>, indices: Vec<u32>) {
+        let index_base = self.vertices.len();
+        self.vertices.extend(vertices);
+        self.indices
+            .extend(indices.iter().map(|i| *i + index_base as u32));
     }
 }
 
@@ -33,11 +49,13 @@ pub fn default_program(display: glium::Display) -> glium::Program {
         in vec2 tex_coords;
         out vec2 v_tex_coords;
 
-        uniform mat4 matrix;
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;
 
         void main() {
             v_tex_coords = tex_coords;
-            gl_Position = matrix * vec4(position, 1.0);
+            gl_Position = projection * view * model * vec4(position, 1.0);
         }
     "#;
 
@@ -55,4 +73,8 @@ pub fn default_program(display: glium::Display) -> glium::Program {
     "#;
 
     glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap()
+}
+
+pub fn degree_to_radian(degree: f32) -> f32 {
+    degree * (3.1415 / 180.)
 }
